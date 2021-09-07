e85b94251723b46231e9ca0d47ac93c35746b681
Hammad Jutt <jutt@ualberta.ca>
2021-09-06 23:08:58 -0600

    Base setup for hardhat + contracts

---
+++ packages/hardhat/scripts/index.ts
@@ -0,0 +1,13 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const accounts = await ethers.provider.listAccounts();
+  console.log(accounts);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
