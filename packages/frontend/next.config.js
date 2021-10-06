131da3fc56128a44df32c0fee361089c9a034495
Hammad Jutt <jutt@ualberta.ca>
2021-10-05 19:25:08 -0600

    Redirect ml01 to index

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -4,4 +4,13 @@ const withTM = require('next-transpile-modules')(['eth-hooks']);
 
 module.exports = withPlugins([withTM], {
   experimental: { esmExternals: true },
+  async redirects() {
+    return [
+      {
+        source: '/ml01',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
 });
