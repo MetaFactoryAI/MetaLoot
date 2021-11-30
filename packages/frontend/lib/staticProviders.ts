0670b7574b3f645a89c49affe6d6d1e7af95ea2a
Hammad Jutt <jutt@ualberta.ca>
2021-11-30 16:51:32 -0700

    hotfix remove pocket

--- packages/frontend/lib/staticProviders.ts
+++ packages/frontend/lib/staticProviders.ts
@@ -4,7 +4,6 @@ import { CONFIG, NETWORKS, TARGET_NETWORK } from '@/config';
 
 export const LocalProvider = getDefaultProvider(TARGET_NETWORK.rpcUrl, {
   infura: CONFIG.infuraId,
-  pocket: CONFIG.poktId,
 });
 
 export const MainnetProvider = new providers.StaticJsonRpcProvider(
