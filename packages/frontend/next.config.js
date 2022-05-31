c18280eae51c7b86576d36ac4da639554190fa33
META DREAMER <jutt@ualberta.ca>
2022-05-30 23:22:33 -0600

    Enable CORS

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -4,6 +4,20 @@ const withTM = require('next-transpile-modules')(['eth-hooks']);
 
 module.exports = withPlugins([withTM], {
   experimental: { esmExternals: true },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   // async redirects() {
   //   return [
   //     {
